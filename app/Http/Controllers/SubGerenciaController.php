137be3f3f92c4e5326689676fb454f62d74faac4
JhobanyT <jhobanyticona@gmail.com>
2024-08-12 17:25:04 -0500

    tables complete

---
+++ app/Http/Controllers/SubGerenciaController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Subgerencia;
+use Illuminate\Http\Request;
+
+class SubGerenciaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Subgerencia $subgerencia)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Subgerencia $subgerencia)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Subgerencia $subgerencia)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Subgerencia $subgerencia)
+    {
+        //
+    }
+}
