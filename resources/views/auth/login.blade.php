82fd0332602448ee3105eade8c1888b131e84ca6
AlanDagner <alanquenaya@upeu.edu.pe>
2025-02-21 11:14:41 -0500

    Proyecto Final s

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -56,8 +56,8 @@
                             <button type="submit" class="boton_inicio"> <i class="fa fa-sign-in fa-1g"
                                     aria-hidden="true"></i>Iniciar Sesión</button>
                         </div>
-                        <p class="text-center creditos">Desarrollado por <strong>Alan Dagner</strong> & <strong>Jhobany
-                                Etduard</strong> &copy; {{ date('Y') }}</p>
+                        <p class="text-center creditos">Desarrollado por <strong>Alan Quenaya</strong> & <strong>Jhobany
+                                Ticona</strong> &copy; {{ date('Y') }}</p>
                     </form>
                 </div>
             </div>
